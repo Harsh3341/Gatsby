d96d817184cd85ccff125351442b64004a24581e
Harsh <harshgupta3341@gmail.com>
2023-02-24 20:02:49 +0530

    Finished Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
+    title: `Using Gatsby Head`,
+    description: `Example project for the Gatsby Head API`,
+    twitterUsername: `@gatsbyjs`,
+    image: `/gatsby-icon.png`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
 }
